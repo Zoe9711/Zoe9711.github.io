9ab16213cf58010efa458258201b2a0e5a2eea90
Zoe Zhang <Hurricane@airbears2-10-142-167-25.airbears2.1918.berkeley.edu>
2019-09-20 23:55:08 -0700

    update

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-;(function ($) {
-  'use strict';
-  var content  = $('#menu').smoothState({
-        // onStart runs as soon as link has been activated
-        onStart : {
-          
-          // Set the duration of our animation
-          duration: 250,
-          
-          // Alterations to the page
-          render: function () {
-
-            // Quickly toggles a class and restarts css animations
-            content.toggleAnimationClass('is-exiting');
-          }
-        }
-      }).data('smoothState'); // makes public methods available
-})(jQuery);
+$("#wow").click(function () {
+    $('.fadeOut').toggleClass('fadeOut-active');
+    $('.darken').toggleClass('darken-active');
+});
