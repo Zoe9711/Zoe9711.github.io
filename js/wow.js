b08225e58d93ad5ee825c6dba0d29bea4fdb4e36
Zoe Zhang <Hurricane@calvisitor-10-105-129-255.calvisitor.1918.berkeley.edu>
2019-10-14 00:39:03 -0700

    update

---
+++ js/wow.js
@@ -0,0 +1,14 @@
+﻿var count = 1
+setInterval(function () {
+    var prev = $('#img-' + count);
+    prev.fadeOut(1200);
+
+
+    count++;
+    if (count > 2) {
+        count = 1
+    }
+
+    var next = $('#img-' + count);
+    next.fadeIn(1200);
+}, 4000);
