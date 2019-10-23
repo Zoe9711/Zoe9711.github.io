224a17dcebd0e6d9d3dfeb3b152038efd3c287e7
Zoe Zhang <Hurricane@airbears2-10-142-166-63.airbears2.1918.berkeley.edu>
2019-10-23 00:06:58 -0700

    upadate

---
+++ js/main.js
@@ -0,0 +1,17 @@
+﻿jQuery(document).ready(function(){
+    typeWriter();
+});
+
+var i = 0;
+var txt = 'Hey, this is Zoe.'; 
+var speed = 50; 
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementsByClassName('transparent-1').innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
