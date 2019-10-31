438cb9ac3708951db635628b08801033eeccacd1
Zoe Zhang <Hurricane@airbears2-10-142-166-147.airbears2.1918.berkeley.edu>
2019-10-31 15:09:45 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 ﻿jQuery(document).ready(function(){
 
 
-    $('body').ripples({
+    $('h1').ripples({
       resolution: 256,
       dropRadius: 20, //px
       perturbance: 0.04,
     });
 
     setInterval(function() {
-      var x = Math.random() * $('body').outerWidth();
-      var y = Math.random() * $('body').outerHeight();
+      var x = Math.random() * $('h1').outerWidth();
+      var y = Math.random() * $('h1').outerHeight();
       var dropRadius = 20;
       var strength = 0.04 + Math.random() * 0.04;
 
-      $('body').ripples('drop', x, y, dropRadius, strength);
-    }, 400);
+      $('h1').ripples('drop', x, y, dropRadius, strength);
+    }, 200);
 
 });
 
